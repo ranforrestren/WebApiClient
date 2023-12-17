f48cf20e6b2c730e70fce40db34099add86a5c13
ranforrestren <ranforrestren@gmail.com>
2023-12-17 12:43:26 -0500

    Add project files.

---
+++ WebApiClient/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApiClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
