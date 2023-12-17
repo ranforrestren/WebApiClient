e07249ca502e7862125736bf7e405830b2e1e494
ranforrestren <ranforrestren@gmail.com>
2023-12-17 17:00:18 -0500

    create HttpClient object and add default Accept and User-Agent headers

--- WebApiClient/Program.cs
+++ WebApiClient/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace WebApiClient
+﻿using System.Diagnostics;
+using System.Net.Http.Headers;
+
+
+namespace WebApiClient
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            using HttpClient client = new();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            await ProcessRepositoriesAsync(client);
+
+            static async Task ProcessRepositoriesAsync(HttpClient client)
+            { 
+            }
         }
     }
 }
