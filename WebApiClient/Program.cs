8781e79bd3b8c677b68b32db728f89560ea9876f
ranforrestren <ranforrestren@gmail.com>
2023-12-17 19:21:18 -0500

    Refactor code to move async code into ProcessRepositoriesAsync

--- WebApiClient/Program.cs
+++ WebApiClient/Program.cs
@@ -15,18 +15,19 @@ namespace WebApiClient
                 new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
             client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
 
-            await ProcessRepositoriesAsync(client);
+            var repositories = await ProcessRepositoriesAsync(client);
+            
+            foreach (var repo in repositories)
+                Console.WriteLine(repo.Name);
+        }
 
-            static async Task ProcessRepositoriesAsync(HttpClient client)
-            {
-                await using Stream stream =
-                    await client.GetStreamAsync("http://api.github.com/orgs/dotnet/repos");
-                var repositories =
-                    await JsonSerializer.DeserializeAsync<List<Repository>>(stream);
-
-                foreach (var repo in repositories ?? Enumerable.Empty<Repository>())
-                    Console.WriteLine(repo.Name);
-            }
+        static async Task<List<Repository>> ProcessRepositoriesAsync(HttpClient client)
+        {
+            await using Stream stream =
+                await client.GetStreamAsync("http://api.github.com/orgs/dotnet/repos");
+            var repositories =
+                await JsonSerializer.DeserializeAsync<List<Repository>>(stream);
+            return repositories ?? new();
         }
     }
 }
